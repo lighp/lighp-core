3ef9286e9c62291b178349fb6a331d0ad1dd0b08
emersion <contact@emersion.fr>
2015-08-24 21:15:29 +0200

    Adds HTTPRequest::referer()

--- src/core/http/HTTPRequest.class.php
+++ src/core/http/HTTPRequest.class.php
@@ -21,8 +21,7 @@ class HTTPRequest {
 	 * @param string $key The cookie's name.
 	 * @return string The cookie's content.
 	 */
-	public function cookieData($key)
-	{
+	public function cookieData($key) {
 		return isset($_COOKIE[$key]) ? $_COOKIE[$key] : null;
 	}
 
@@ -31,8 +30,7 @@ class HTTPRequest {
 	 * @param string $key The cookie's name.
 	 * @return bool True if it exists, false otherwise.
 	 */
-	public function cookieExists($key)
-	{
+	public function cookieExists($key) {
 		return isset($_COOKIE[$key]);
 	}
 
@@ -41,8 +39,7 @@ class HTTPRequest {
 	 * @param string $key The data name.
 	 * @return string The data's content.
 	 */
-	public function getData($key)
-	{
+	public function getData($key) {
 		return isset($_GET[$key]) ? $_GET[$key] : null;
 	}
 
@@ -51,8 +48,7 @@ class HTTPRequest {
 	 * @param string $key The data name.
 	 * @return bool True if the data exists, false otherwise.
 	 */
-	public function getExists($key)
-	{
+	public function getExists($key) {
 		return isset($_GET[$key]);
 	}
 
@@ -61,8 +57,7 @@ class HTTPRequest {
 	 * @param string $key The data name.
 	 * @return string The data's content.
 	 */
-	public function postData($key)
-	{
+	public function postData($key) {
 		return isset($_POST[$key]) ? $_POST[$key] : null;
 	}
 
@@ -71,8 +66,7 @@ class HTTPRequest {
 	 * @param string $key The data name.
 	 * @return bool True if the data exists, false otherwise.
 	 */
-	public function postExists($key)
-	{
+	public function postExists($key) {
 		return isset($_POST[$key]);
 	}
 
@@ -80,9 +74,16 @@ class HTTPRequest {
 	 * Get this request's URI.
 	 * @return string
 	 */
-	public function requestURI()
-	{
+	public function requestURI() {
 		return $_SERVER['REQUEST_URI'];
+	}
+
+	/**
+	 * Get this request's HTTP referer.
+	 * @return string
+	 */
+	public function referer() {
+		return (isset($_SERVER['HTTP_REFERER'])) ? $_SERVER['HTTP_REFERER'] : null;
 	}
 
 	/**
